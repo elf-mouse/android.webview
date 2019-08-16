4b31d7c5f47ddad8ca37a5748eda9280b104af27
Elf-mousE <ifmouz@gmail.com>
2019-08-16 15:43:30 +0800

    update

--- app/src/main/java/org/skyfox/android_webview_javascript/MainActivity.java
+++ app/src/main/java/org/skyfox/android_webview_javascript/MainActivity.java
@@ -22,23 +22,20 @@ public class MainActivity extends AppCompatActivity {
         WebView webview = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webview.setWebChromeClient(new WebChromeClient()); //webview只是一个承载体，各种内容的渲染需要使用webviewChromClient去实现，所以set一个默认的基类WebChromeClient就行
-        webview.addJavascriptInterface(this,"Native");  //注入Native对象到webview window中
+        webview.setWebChromeClient(new WebChromeClient()); // webview只是一个承载体，各种内容的渲染需要使用webviewChromClient去实现，所以set一个默认的基类WebChromeClient就行
+        webview.addJavascriptInterface(this,"Native");  // 注入Native对象到webview window中
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             WebView.setWebContentsDebuggingEnabled(true);
         }
 
-      webview.loadUrl("file:///android_asset/JSWebView.html");
-
-
+        webview.loadUrl("file:///android_asset/JSWebView.html");
 //        webview.loadUrl("http://dev.skyfox.org/JSWebView.html");
-//        webView.loadUrl("javascript:alert(Native.alert())");
+//        webview.loadUrl("javascript:alert(Native.alert())");
     }
+
     @JavascriptInterface
     public void alert(String param){
         Log.i("A",param.toString());
-
-
     }
 }
